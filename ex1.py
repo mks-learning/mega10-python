dc0bedf40106311f0ccbd211ba8382d3486ce273
MKS <kmrajbills@gmail.com>
2020-07-21 21:39:24 -0700

    optimized

--- ex1.py
+++ ex1.py
@@ -1,5 +1,5 @@
 # student_grade = [9.1, 8.8, 7.5]
-directory = [list, float, str, int, __builtins__, dict]
+directory = [list, float, str, int, __builtins__, dict, tuple]
 f = open('dir_of_python_items.txt', 'w')
 for i in range(0, len(directory)):
     f.write('\n \n \n')
