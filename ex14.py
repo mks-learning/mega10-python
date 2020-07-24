665f29def3efa3048098b32064ef99858a2a4d7d
MKS <kmrajbills@gmail.com>
2020-07-23 21:35:23 -0700

    more exercises

---
+++ ex14.py
@@ -0,0 +1,17 @@
+seconds = [1.2323442655, 1.4534345567, 1.023458894, 1.10001399445]
+dropme = [1.4534345567, 1.023458894, 1.10001399445]
+for i in range(0, len(seconds)):
+
+
+
+
+
+
+# print(i, dropme.index(i))
+# dex = seconds.index(dropme.value[i])
+# seconds.drop(dex)
+# print(seconds)
+
+
+# seconds.remove(val)
+# print(seconds)
---
+++ section4.py
@@ -0,0 +1,10 @@
+seconds = [1.2323442655, 1.4534345567, 1.023458894, 1.10001399445]
+dropme = [1.4534345567, 1.023458894, 1.10001399445]
+print(seconds)
+print(dropme)
+for i in range(0,len(seconds)):
+    print(dropme[i])
+    print(seconds[i])
+#    if dropme[i] in ()
+    seconds.remove(se)
+print(seconds)
