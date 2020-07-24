ddfda4e5eb787ff661548936bf2e3b2ab97aec89
MKS <kmrajbills@gmail.com>
2020-07-23 19:55:51 -0700

    fixed and further optimized ex1

--- ex1.py
+++ ex1.py
@@ -1,8 +1,7 @@
 # student_grade = [9.1, 8.8, 7.5]
 directory = [list, float, str, int, __builtins__, dict, tuple]
-f = open('dir_of_python_items.txt', 'w')
+f = open('dir_of_python_classes.txt', 'w')
 for i in range(0, len(directory)):
-    f.write('\n \n \n')
-    f.write('Print output of the contents of ' + str(directory[i]) + '\n')
-    f.write(str(dir(i)))
+    f.write('\n \n \n Print output of ' + str(directory[i]) + ':\n')
+    f.write(str(dir(directory[i])))
 f.close()
