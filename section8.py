dfc7cb32a8e8fd1e9767aa3039c812fef4817efc
MKS <kmrajbills@gmail.com>
2020-07-26 13:06:12 -0700

    new files

---
+++ section8.py
@@ -0,0 +1,6 @@
+# objective is to create a program that allows the User
+# to enter a phrase. If the phrase is not /end it continues
+# if it continues it adds the phrase as an element in a list
+# data structure. Once the /end is encountered. the while
+# loop is broken and the entire contents of the list is
+# printed out in capitalize format.
