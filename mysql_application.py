e1c7a38fccfaacdb3dd2087cbda2aa6858f19b82
MKS <kmrajbills@gmail.com>
2020-07-29 18:37:49 -0700

    finished SQL exercise!!!

--- mysql_application.py
+++ mysql_application.py
@@ -8,56 +8,38 @@ password="ardit700_student",
 host="108.167.140.122",
 database="ardit700_pm1database"
 )
+word = input('What word would you like the definition? ')
 cursor = con.cursor()
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % word)
+specific = cursor.fetchall()
+# print(specific)
+query_all = cursor.execute("SELECT * FROM Dictionary")
+all = cursor.fetchall()
+otherwords = get_close_matches(word, (val[0] for val in all))
 
-
-def getanswers(word):
-    cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % word)
-    results = cursor.fetchall()
-    if len(results) > 0:
-        return results[0]
-    else:
-        return 'Null'
 
 
 def getdef(word):
-    results = []
-    if len(getanswers(word)) > 0 and getanswers(word) != 'Null':
-        results = getanswers(word)
-        return results
-    elif len(getanswers(word.title())) > 0 and getanswers(word) != 'Null':
-        results = getanswers(word)
-        return results
-    elif len(getanswers(word.upper())) > 0 and getanswers(word) != 'Null':
-        results = getanswers(word)
-        return results
-    elif len(get_close_matches(word, getanswers(word))) > 0:
-        answer = input('Did you mean %s ? (y/n)' % results)
+    if len(specific) > 0:
+        return specific
+    elif len(get_close_matches(word, (val[0] for val in all))) > 0:
+        answer = input('Did you mean %s ? (y/n) ' % otherwords[0])
         if answer == 'y':
-            results = get_close_matches(word, getanswers(word))
-            print('using get_close_matches, ' +
-                  [get_close_matches(word, results)])
-            return [get_close_matches(word, results)]
+            cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % otherwords[0])
+            local = cursor.fetchall()
+            return local
         else:
-            return "Didn\'t understand your entry. Try again."
+            nothing = "Didn\'t understand your entry. Try again."
+            return nothing
     else:
-        return "Didn\'t find your entry. Try again."
+        none = "Didn\'t find your entry. Try again."
+        return none
 
 
-word = 'Delhi'
-print(getdef(word))
+answer = getdef(word)
 
-# cursor = con.cursor()
-# results = ''
-# word = input('What word would you like the definition? ')
-# answer = getdef(word)
-# # query = cursor.execute('SELECT * FROM Dictionary WHERE Expression = \'%s\''
-# #         % word)
-# # results = cursor.fetchall()
-#
-#
-# if results:
-#     for result in results:
-#         print(result[1])
-# else:
-#     print('No word found!')
+if type(answer) == list:
+    for result in answer:
+        print(result[1])
+else:
+    print('No word found!')
