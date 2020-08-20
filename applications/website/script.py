73e9fdb3d7cb7bf274419b4dfea479f2a201d9c3
MKS <kmrajbills@gmail.com>
2020-08-19 22:25:05 -0700

    created web pages

---
+++ applications/website/script.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+# the route('/') is referring to the page within the website. So '/' means
+# root. And '/about/' would be root/about
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about/')
+# the route('/') is referring to the page within the website. So '/' means
+# root. And '/about/' would be root/about
+def contact():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
