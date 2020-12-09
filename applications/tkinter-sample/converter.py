117e6aa45c1e08aba2b2d5f0b768421e061e9342
Mukund Sreenivasan <mukund@MrPeabody.local>
2020-12-08 19:14:55 -0800

    added sample conversion app

---
+++ applications/tkinter-sample/converter.py
@@ -0,0 +1,9 @@
+import from tkinter *
+
+# Create a Python program that expects a kilogram input value and converts
+#  that value to grams, pounds, and ounces when the user pushes the Convert button.   
+
+conwindow=Tk()
+
+
+
