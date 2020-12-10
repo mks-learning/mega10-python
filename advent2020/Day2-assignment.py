dadace7c9ec9618d6bbd5fe5d0eaedd27ed9a97b
Mukund Sreenivasan <mukund@MrPeabody.local>
2020-12-10 15:22:06 -0800

    finished day 2 assignments

--- advent2020/Day2-assignment.py
+++ advent2020/Day2-assignment.py
@@ -3,6 +3,9 @@ import pandas as pd
 # read in the content of the CSV with a space delimit and then convert it to a list and 
 # then split the list into more lists by index. so [A1, B1, C1] [A2, B2, C2], etc becomes
 # [A1, A2, A3] and then [B1, B2, B3] etc.
+# THERE ARE TWO TESTS...test one is the the validity of the counts of the test string
+# The second test is the position of the test string in the password. Commented text
+# in the body of the code is the first test
 
 foo = pd.read_csv('Day2-input.csv', delim_whitespace=True)
 bar = [list(row) for row in foo.values]
@@ -16,16 +19,20 @@ while rowNum < len(bar):
     temptest = temptest.split(':')
     low = rule[0]
     high= rule[1]
-    str_test = tempassword.count(str(temptest[0]))
-    str_test_low = (temptest[0] == tempassword[int(low)-1])
-    str_test_high = ((temptest[0] == tempassword[int(high)-1]))
+    # str_test = tempassword.count(str(temptest[0]))
+    low_test = tempassword[int(low)-1]
+    high_test = tempassword[int(high)-1]
+    test_low = (temptest[0] == low_test)
+    test_high = ((temptest[0] == high_test))
+ 
+    # test for first part of Day two assignment
+    # # if (int(low) <= int(str_test) <= int(high)):
+    #     i += 1
+    # #rowNum += 1
 
-    if (int(low) <= int(str_test) <= int(high)):
-        i += 1
-    #rowNum += 1
-
-  
-    if (int(low) <= int(str_test) <= int(high)) and str_test_low and str_test_high:
+    if ((test_low) and (test_high == False)):
+        trueCount += 1
+    elif ((test_low == False) and (test_high)):
         trueCount += 1
     rowNum += 1
-print("The final answer to the question 1 is "+str(i)+" and the final answer to question 2 is "+str(trueCount)+".")
+print("The final answer to question 2 is "+str(trueCount)+".")
