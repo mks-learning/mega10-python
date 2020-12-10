fac6f3fcad4105fa2ec0b255af2af104a146f0dc
Mukund Sreenivasan <mukund@MrPeabody.local>
2020-12-09 20:52:35 -0800

    added additonal look for part 2 of question.

--- advent2020/Day1-1stAssignment.py
+++ advent2020/Day1-1stAssignment.py
@@ -1,21 +1,24 @@
 import csv
 
 # reads in the input file and then converts it to integer value.
-with open('stars_input.csv', 'r') as listObj:
+with open('Day1-stars_input.csv', 'r') as listObj:
     readin = csv.reader(listObj)
     rows = [int(row[0]) for row in readin]
 
 # debug print command to make sure the content was read.
 # print(rows)
 
-i, j, answer = 0, 0, 2020
+i, j, k, answer = 0, 0, 0, 2020
 while i < len(rows):
     while j < len(rows):
-        if rows[i]+rows[j] == answer:
-            print(rows[i]+rows[j])
-            multanswer = rows[i]*rows[j]
-            print(str(multanswer)+' is the value for the list items '+str(rows[i])+" "+str(rows[j]))
-            quit()
+        while k < len(rows):
+            if rows[i]+rows[j]+rows[k] == answer:
+                print(rows[i]+rows[j]+rows[k])
+                multanswer = rows[i]*rows[j]*rows[k]
+                print(str(multanswer)+' is the value for the list items ' + str(rows[i])+" "+str(rows[j])+" "+str(rows[k]))
+                quit()
+            k += 1
+        k=0
         j += 1
     j = 0
     i += 1
